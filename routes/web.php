2865344480aaf923a89a54fd0b477753f3039d19
SofianeAbouSafwan <104463396+SofianeAbouSafwan@users.noreply.github.com>
2022-05-07 17:49:57 +0100

    Implementing the Actions To Start the Subscription of a User

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\HomeController;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/payments/pay', [PaymentController::class, 'pay'])->name('pay');
+Route::get('/payments/approval', [PaymentController::class, 'approval'])->name('approval');
+Route::get('/payments/cancelled', [PaymentController::class, 'cancelled'])->name('cancelled');
+
+Route::prefix('subscribe')
+    ->name('subscribe.')
+    ->group(function () {
+        Route::get('/', [SubscriptionController::class, 'show'])
+            ->name('show');
+
+        Route::post('/', [SubscriptionController::class, 'store'])
+            ->name('store');
+
+        Route::get('/approval', [SubscriptionController::class, 'approval'])
+            ->name('approval');
+
+        Route::get('/cancelled', [SubscriptionController::class, 'cancelled'])
+            ->name('cancelled');
+    });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
